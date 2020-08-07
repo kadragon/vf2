88dde61163048f60892f5610506ff1422d31a744
kangdonguk <kangdongouk@gmail.com>
2020-08-08 08:52:29 +0900

    -index.vue test link config

--- src/router/index.js
+++ src/router/index.js
@@ -18,11 +18,6 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
-  },
-  {
-    path: "/Link",
-    name: "Link",
-    component: () => import("../views/Link.vue")
   }
 ];
 
