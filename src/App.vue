fbda12d47b3b2cac627a435b0029db0057216daa
kadragon <kangdongouk@gmail.com>
2020-08-09 23:32:03 +0900

    14 +Expansion Lists

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,6 @@
     </v-app-bar>
 
     <v-navigation-drawer app absolute temporary v-model="drawer">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">Application</v-list-item-title>
-          <v-list-item-subtitle>subtext</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
       <site-Menu />
     </v-navigation-drawer>
 
--- src/components/site/Menu.vue
+++ src/components/site/Menu.vue
@@ -1,15 +1,40 @@
 <template>
-  <v-list dense nav>
-    <v-list-item v-for="item in items" :key="item.title" :to="item.to">
-      <v-list-item-icon>
-        <v-icon>{{ item.icon }}</v-icon>
-      </v-list-item-icon>
-
+  <div>
+    <v-list-item>
       <v-list-item-content>
-        <v-list-item-title>{{ item.title }}</v-list-item-title>
+        <v-list-item-title class="title">Application</v-list-item-title>
+        <v-list-item-subtitle>subtext</v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
-  </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list>
+      <v-list-group
+        v-for="(item, i) in items"
+        :key="i"
+        v-model="item.active"
+        :prepend-icon="item.icon"
+        no-action
+      >
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </template>
+
+        <v-list-item
+          v-for="subItem in item.items"
+          :key="subItem.title"
+          :to="subItem.to"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="subItem.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-group>
+    </v-list>
+  </div>
 </template>
 
 <script>
@@ -17,9 +42,31 @@ export default {
   data() {
     return {
       items: [
-        { title: "Dashboard", icon: "mdi-view-dashboard", to: "/" },
-        { title: "Photos", icon: "mdi-image", to: "/about" },
-        { title: "About", icon: "mdi-help-box", to: "/" }
+        {
+          title: "HOME",
+          icon: "mdi-home",
+          items: [
+            {
+              title: "Dashboard",
+              to: "/"
+            },
+            {
+              title: "About",
+              to: "/about"
+            }
+          ]
+        },
+        {
+          title: "About",
+          icon: "mdi-account",
+          active: true,
+          items: [
+            {
+              title: "About",
+              to: "/About"
+            }
+          ]
+        }
       ]
     };
   }
