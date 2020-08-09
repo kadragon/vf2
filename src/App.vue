19a4d769c21a620d3918da19eddcafa01e888ce6
kadragon <kangdongouk@gmail.com>
2020-08-09 23:04:04 +0900

    12 +components

--- src/App.vue
+++ src/App.vue
@@ -2,39 +2,48 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>Title</v-toolbar-title>
+      <site-title :title="title" />
       <v-spacer />
       <v-btn icon to="/about">
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
 
-    <v-navigation-drawer temporary v-model="drawer">
+    <v-navigation-drawer app absolute temporary v-model="drawer">
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="title">Application</v-list-item-title>
           <v-list-item-subtitle>subtext</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
+
+      <v-divider></v-divider>
+
+      <site-Menu />
     </v-navigation-drawer>
 
     <v-content>
       <router-view></router-view>
     </v-content>
 
-    <v-footer app color="primary" dark absolute="true">
-      <v-spacer></v-spacer>
-      <div>&copy; {{ new Date().getFullYear() }}</div>
-    </v-footer>
+    <site-footer :footer="footer" />
   </v-app>
 </template>
 
 <script>
+import SiteTitle from "@/components/site/Title";
+import SiteFooter from "@/components/site/Footer";
+import SiteMenu from "@/components/site/Menu";
+
 export default {
+  components: { SiteTitle, SiteFooter, SiteMenu },
   name: "App",
   data() {
     return {
-      drawer: false
+      drawer: null,
+      items: [],
+      title: "Vue+Firebase",
+      footer: "kadragon"
     };
   }
 };
---
+++ src/components/site/Menu.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-list dense nav>
+    <v-list-item v-for="item in items" :key="item.title" :to="item.to">
+      <v-list-item-icon>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-item-icon>
+
+      <v-list-item-content>
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard", to: "/" },
+        { title: "Photos", icon: "mdi-image", to: "/about" },
+        { title: "About", icon: "mdi-help-box", to: "/" }
+      ]
+    };
+  }
+};
+</script>
