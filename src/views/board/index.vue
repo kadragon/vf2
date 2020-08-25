9167a1cd787f1cc08e46e356be5ec8ba469eb2fc
kangdonguk <kangdongouk@gmail.com>
2020-08-25 09:45:02 +0900

    29

--- src/views/board/index.vue
+++ src/views/board/index.vue
@@ -55,13 +55,38 @@ export default {
         content: ""
       },
       dialog: false,
-      selectedItem: null
+      selectedItem: null,
+      unsubscribe: null
     };
   },
   created() {
-    this.read();
+    // this.read();
+    this.subscribe();
+  },
+  destroyed() {
+    if (this.unsubscribe) this.unsubscribe();
   },
   methods: {
+    subscribe() {
+      this.unsubscribe = this.$firebase
+        .firestore()
+        .collection("boards")
+        .onSnapshot(sn => {
+          if (sn.empty) {
+            this.items = [];
+            return;
+          }
+          this.items = sn.docs.map(v => {
+            const item = v.data();
+            return {
+              id: v.id,
+              title: item.title,
+              content: item.content
+            };
+          });
+          console.log(this.items);
+        });
+    },
     openDialog(item) {
       this.selectedItem = item;
       this.dialog = true;
