bb07ed62370e103c3446298e2194f1d89966e192
kangdonguk <kangdongouk@gmail.com>
2020-08-28 19:47:52 +0900

    34

--- src/views/Storage.vue
+++ src/views/Storage.vue
@@ -1,14 +1,19 @@
 <template>
   <v-sheet>
+    <v-textarea v-model="text"></v-textarea>
     <v-btn @click="write">write</v-btn>
+    <v-btn @click="read">read</v-btn>
   </v-sheet>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
-      text: ""
+      text: "",
+      url: ""
     };
   },
   methods: {
@@ -19,7 +24,12 @@ export default {
         .child("xxx.txt")
         .putString("abcd");
       const url = await sn.ref.getDownloadURL();
-      console.log(url);
+      this.url = url;
+    },
+    async read() {
+      const r = await axios.get(this.url);
+      console.log(r);
+      this.text = r.data;
     }
   }
 };
